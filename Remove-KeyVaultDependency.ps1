8c3db5d205fe29c44fff1f09fcd22cb9dc8d4cde
hynesight <craig@ozbeanz.com>
2017-06-29 11:28:11 +1000

    Add file via upload

---
+++ Remove-KeyVaultDependency.ps1
@@ -0,0 +1,5 @@
+﻿$vmName = "[VMNAME]"
+$rgName = "[RESOURCEGROUPNAME]"
+$vm = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+$vm.OSProfile.Secrets = New-Object -TypeName "System.Collections.Generic.List[Microsoft.Azure.Management.Compute.Models.VaultSecretGroup]" 
+Update-AzureRmVM -ResourceGroupName $rgName -VM $vm -Debug
