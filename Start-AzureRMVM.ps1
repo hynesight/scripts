d0c689535a612a7ce7c26eea03ee22d3d001fea3
hynesight <craig@ozbeanz.com>
2017-06-29 11:26:59 +1000

    Add files via upload

---
+++ Start-AzureRMVM.ps1
@@ -0,0 +1,19 @@
+﻿workflow Start-AzureRMVM
+{
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceGroupName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]
+    $VM
+)
+$Cred = Get-AutomationPSCredential –Name '[AUTOMATION CREDENTIAL NAME]'
+
+Add-AzureRMAccount -Credential $Cred
+
+Select-AzureRMSubscription '[SUBSCRIPTION NAME]'
+
+Start-AzureRMVM -ResourceGroupName $ResourceGroupName -Name $VM
+}
---
+++ Stop-AzureRMVM.ps1
@@ -0,0 +1,19 @@
+﻿workflow Stop-AzureRMVM
+{
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceGroupName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]
+    $VM
+)	
+    $Cred = Get-AutomationPSCredential –Name '[AUTOMATION CREDENTIAL NAME]'
+
+    Add-AzureRMAccount -Credential $Cred
+
+    Select-AzureRMSubscription '[SUBSCRIPTION NAME]'
+    
+    Stop-AzureRMVM -ResourceGroupName $ResourceGroupName -Name $VM -force
+}
